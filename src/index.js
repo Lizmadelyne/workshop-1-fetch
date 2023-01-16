09672f975f15e7b5cb10d1449376c9bf3c5d1f5c
Lizmadelyne <lizfajardop@gmail.com>
2023-01-16 07:23:41 -0500

    everything is ok

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@
  **/
 
 const baseUrl = "https://platzi-avo.vercel.app";
-const appNode = document.querySelector('#app')
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) =>{
+   const newPrice = new window.Intl.NumberFormat("en-En",{
+    style: "currency",
+    currency :"USD",
+   }).format(price);
+   return newPrice
+};
 //web api estoy usando promesas
 //aqui conecto al servidor
 window
@@ -15,19 +23,36 @@ window
 .then((responsejson)=>{
       // esto lo cree de ultimo para que funcione
       const todosLosItems =[];
+
     responsejson.data.forEach(item => { 
         //crear la imagen
         const imagen = document.createElement('img');
         imagen.src =`${baseUrl}${item.image}`;
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
                 // crear un titulo
         const title = document.createElement('h2');
+        title.className = "text-lg";
         title.textContent = item.name;
+       
         //crear precio
         const price = document.createElement('div');
-        price.textContent = item.price;
+        price.className = "text-gray-600";
+        price.textContent = formatPrice(item.price); //esto al final aon api internacionalizacion
+
+        //se crea un contenedor para titulo y precio
+        const PriceAndTitle = document.createElement("div");
+        PriceAndTitle.className= "text-center md:text-left";
+        PriceAndTitle.appendChild(title);
+        PriceAndTitle.appendChild(price);
+
+        //metemos todo dentro de una tarjeta contenedora
+        const card = document.createElement("div");
+        card.className =  "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+        card.append(imagen, PriceAndTitle);
+     
         //crear el contenedor que va a ir dentro del body
-        const container = document.createElement('div');
-        container.append(imagen, title, price);
+       const container = document.createElement('div');
+        container.appendChild(card);
 
         todosLosItems.push(container);
      
